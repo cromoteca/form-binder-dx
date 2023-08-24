db11952bd49a21b75bed7fe957e861bc43ec8647
Luciano Vernaschi <luciano@vaadin.com>
2023-08-24 09:14:54 +0200

    Validation with server-side bean validation annotations

--- src/main/java/com/example/application/backend/Employee.java
+++ src/main/java/com/example/application/backend/Employee.java
@@ -1,5 +1,8 @@
 package com.example.application.backend;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class Employee {
     private static long id_tracker = 42L; 
 
@@ -24,6 +27,8 @@ public class Employee {
 
     }
 
+    @NotBlank(message = "First name is mandatory")
+    @Size(max = 10, message = "First name cannot be longer than 10 characters")
     public String getFirstname() {
         return firstname;
     }
@@ -32,6 +37,8 @@ public class Employee {
         this.firstname = firstname;
     }
 
+    @NotBlank(message = "Last name is mandatory")
+    @Size(max = 10, message = "Last name cannot be longer than 10 characters")
     public String getLastname() {
         return lastname;
     }
@@ -48,6 +55,7 @@ public class Employee {
         this.title = title;
     }
 
+    @NotBlank(message = "Email is mandatory")
     public String getEmail() {
         return email;
     }
